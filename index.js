7e2977e86dca0573b4f7bc159dc4697fa58d15e0
VNetoJunior <volneyjunior000@outlook.com>
2024-10-21 15:34:40 -0300

    Adicioando Total de Partidas no mês!

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 
 let nomeUsuario = "Junior"
 
-let vitoriasNaSemana = [14, 5, 15, 8]
+let vitoriasNaSemana = [40, 10, 56, 30]
 
-let derrotasNaSemana = [7, 5, 10, 4]
+let derrotasNaSemana = [10, 4, 12, 23]
 
 let totalVitoriasMes = vitoriasNaSemana[0] + vitoriasNaSemana[1] + vitoriasNaSemana[2] + vitoriasNaSemana[3]
 
@@ -18,12 +18,18 @@ let totalPartidasSemana = [
     vitoriasNaSemana[3] + derrotasNaSemana[3]
 ]
 
+let totalPartidasMes = totalPartidasSemana[0] + totalPartidasSemana[1] + totalPartidasSemana[2] + totalPartidasSemana[3]
+
 
 console.log (`
     O usuário de nome ${nomeUsuario}, jogou patidas do seu jogo favorito por um mês e esses foram seus resultados: 
+    
     Na Primeira semana, ${nomeUsuario} jogou ${totalPartidasSemana[0]} Partidas, Vencendo ${vitoriasNaSemana[0]} e Perdendo ${derrotasNaSemana[0]}!
+    
     Na Segunda semana, ele não conseguiu jogar muito, pois estava ocupado, portanto, jogou ${totalPartidasSemana[1]} Partidas, Vencendo ${vitoriasNaSemana[1]} e Perdendo ${derrotasNaSemana[1]}!
+    
     Na Terceira semana, ${nomeUsuario} estava motivado pra pegar um bom Rank e jogou ${totalPartidasSemana[2]} Partidas, Vencendo ${vitoriasNaSemana[2]} e Perdendo ${derrotasNaSemana[2]}!
+    
     E por fim, em sua última semana do mês, ele jogou ${totalPartidasSemana[3]} Partidas, Venceu ${vitoriasNaSemana[3]} e Perdeu ${derrotasNaSemana[3]}!
 `)
 
@@ -45,23 +51,23 @@ switch(true) {
         ranking = "Ferro!"
         break
 
-    case numeroDeVitorias >= 11 || numeroDeVitorias <= 20:
+    case numeroDeVitorias >= 11 && numeroDeVitorias <= 20:
         ranking = "Bronze!"
         break
 
-    case numeroDeVitorias >= 21 || numeroDeVitorias <= 50:
+    case numeroDeVitorias >= 21 && numeroDeVitorias <= 50:
         ranking = "Prata!"
         break
 
-    case numeroDeVitorias >= 51 || numeroDeVitorias <= 80:
+    case numeroDeVitorias >= 51 && numeroDeVitorias <= 80:
         ranking = "Ouro!"
         break
 
-    case numeroDeVitorias >= 81 || numeroDeVitorias <= 90:
+    case numeroDeVitorias >= 81 && numeroDeVitorias <= 90:
         ranking = "Diamante!"
         break
 
-    case numeroDeVitorias >= 91 || numeroDeVitorias <= 100:
+    case numeroDeVitorias >= 91 && numeroDeVitorias <= 100:
         ranking = "Lendário!"
         break
 
@@ -74,6 +80,12 @@ switch(true) {
         break
 }
 
+
+
+
+console.log (`
+    Após um mês jogando, ${nomeUsuario} jogou ${totalPartidasMes} Partidas, Vencendo ${totalVitoriasMes} e Perdendo ${totalDerrotasMes}!
+`)
 
 console.log (`
     Portanto, ${nomeUsuario} tem saldo de ${numeroDeVitorias} vitórias, ficando com o ranking de ${ranking}
